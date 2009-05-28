cda91e444e43d1d42d712d6c00bb62f003e5bf1d
Dion Moult <dion@thinkmoult.com>
2009-05-28 18:06:05 +0800

    We now have a core controller.
     * Use the docblock seen at the top of that page for ALL pages.
     * All future controllers will extend this one.
     * Category will ALWAYS be "Eadrax"
     * Package will be either:
      - User
      - Project
      - Update
      - Dashboard
      - Profile
      - Core
     * Subpackage will be either
      - Controllers
      - Models
      - Views

---
+++ application/controllers/core.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Eadrax
+ *
+ * Eadrax is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * Eadrax is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *                                                                                
+ * You should have received a copy of the GNU General Public License
+ * along with Eadrax; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @category	Eadrax
+ * @package		Core
+ * @author		Eadrax Team
+ * @copyright	Copyright (C) 2009 Eadrax Team
+ */
+
+/**
+ *
+ * Core controller that everything uses.
+ *
+ * Sets some globally required methods and variables.
+ *
+ * @category	Eadrax
+ * @package		Core
+ * @subpackage	Controllers
+ * @author		Eadrax Team
+ * @copyright	Copyright (C) 2009 Eadrax Team
+ * @version		$Id$
+ */
+abstract class Core_Controller extends Template_Controller {
+	// We do not yet have a template.
+	public $template	= 'none';
+	public $site_name	= 'Eadrax';
+	public $slogan		= 'Totally awesome.';
+
+	/**
+	 * Set up routine.
+	 *
+	 * @return null
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
