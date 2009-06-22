7924c7b20206c9e86710d6fd8d9e297e3ef31c6e
Andrew <easylancer@gmail.com>
2009-06-22 15:26:04 +0100

    Removed the log file from the logs directory

--- application/logs/2009-06-22.log.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2009-06-22 15:16:22 +01:00 --- error: Uncaught PHP Error: mkdir() [<a href='function.mkdir'>function.mkdir</a>]: No such file or directory in file E:/learn/eadrax/application/controllers/openid.php on line 102
