f1630b4d3c2d4ee3cef195c341ec875d8243a8a6
Dion Moult <dion@thinkmoult.com>
2009-07-12 17:49:39 +0800

    Merged the add/edit project functions to minimise code duplication.

--- application/controllers/projects.php
+++ application/controllers/projects.php
@@ -35,11 +35,14 @@
  */
 class Projects_Controller extends Core_Controller {
 	/**
-	 * Process to add a new project.
+	 * Process to add/edit a project.
+	 *
+	 * @param int $pid If a project ID is specified, we will edit instead of 
+	 * adding a project.
 	 *
 	 * @return null
 	 */
-	public function add()
+	public function add($pid = FALSE)
 	{
 		// Only logged in users are allowed.
 		$this->restrict_access();
@@ -47,128 +50,11 @@ class Projects_Controller extends Core_Controller {
 		// Load necessary models.
 		$project_model = new Project_Model;
 
-		if ($this->input->post())
+		// If a $pid is specified, this means we are editing a project, hence we 
+		// need to perform some authentication checks.
+		if ($pid != FALSE)
 		{
-			$name			= $this->input->post('name');
-			$website		= $this->input->post('website');
-			$contributors	= $this->input->post('contributors');
-			$description	= $this->input->post('description');
-			$cid			= $this->input->post('cid');
-
-			// When adding a new project, there is no icon to start off with. If 
-			// we detect a file for the icon later on, we will replace this.
-			$icon_filename	= '';
-
-			$validate = new Validation($this->input->post());
-			$validate->pre_filter('trim');
-			$validate->add_rules('name', 'required', 'length[1, 25]', 'standard_text');
-			$validate->add_rules('website', 'url');
-			$validate->add_rules('contributors', 'standard_text');
-			$validate->add_rules('description', 'required');
-			$validate->add_rules('cid', 'required', 'between[1, '. Kohana::config('projects.max_cid') .']');
-
-			if ($validate->validate())
-			{
-				// First check whether or not we even have an icon to validate.
-				if (!empty($_FILES) && !empty($_FILES['icon']['name']))
-				{
-					// Do not forget we need to validate the file.
-					$files = new Validation($_FILES);
-					$files = $files->add_rules('icon', 'upload::valid', 'upload::type[jpg,png]', 'upload::size[1M]');
-
-					if ($files->validate())
-					{
-						// Upload and resize the image.
-						$filename = upload::save('icon');
-						Image::factory($filename)->resize(80, 80, Image::WIDTH)->save(DOCROOT .'uploads/icons/'. basename($filename));
-						unlink($filename);
-						$icon_filename = basename($filename);
-					}
-					else
-					{
-						die ('Your upload has failed.');
-					}
-				}
-				
-				// Everything went great! Let's add the project.
-				$project_model->manage_project(array(
-					'uid'			=> $this->uid,
-					'cid'			=> $cid,
-					'name'			=> $name,
-					'website'		=> $website,
-					'contributors'	=> $contributors,
-					'description'	=> $description,
-					'icon'			=> $icon_filename
-					));
-
-				// Then load our success view.
-				$project_success_view = new View('project_success');
-
-				// Then generate content.
-				$this->template->content = array($project_success_view);
-			}
-			else
-			{
-				// Errors have occured. Fill in the form and set errors.
-				$project_form_view = new View('project_form');
-				$project_form_view->form = arr::overwrite(array(
-					'name' => '',
-					'website' => '',
-					'contributors' => '',
-					'description' => ''
-					), $validate->as_array());
-				$project_form_view->errors = $validate->errors('project_errors');
-
-				// Set project categories.
-				$project_form_view->categories = $project_model->categories();
-
-				// Generate the content.
-				$this->template->content = array($project_form_view);
-			}
-		}
-		else
-		{
-			// Load the neccessary view.
-			$project_form_view = new View('project_form');
-
-			// If we didn't press submit, we want a blank form.
-			$project_form_view->form = array(
-				'name' => '',
-				'website' => '',
-				'contributors' => '',
-				'description' => ''
-			);
-
-			// Set project categories.
-			$project_form_view->categories = $project_model->categories();
-
-			// Generate the content.
-			$this->template->content = array($project_form_view);
-		}
-
-	}
-
-	/**
-	 * Process to edit a project.
-	 *
-	 * @return null
-	 */
-	public function edit($pid = FALSE)
-	{
-		// Only logged in users are allowed.
-		$this->restrict_access();
-
-		// Load necessary models.
-		$project_model = new Project_Model;
-
-		// We need a $pid to operate!
-		if ($pid == FALSE)
-		{
-			die('Please specify a project ID.'); # TODO dying isn't very good.
-		}
-		else
-		{
-			// check whether or not we own the project.
+			// Check whether or not we own the project.
 			if (!$project_model->check_project_owner($pid, $this->uid))
 			{
 				die('You do not own this project.'); # TODO dying isn't very good.
@@ -183,10 +69,17 @@ class Projects_Controller extends Core_Controller {
 			$description	= $this->input->post('description');
 			$cid			= $this->input->post('cid');
 
-			// The icon_filename will only change if we have selected a new file 
-			// to upload. It will replace the old one.
-			$icon_filename	= $project_model->project_information($pid);
-			$icon_filename	= $icon_filename['icon'];
+			if ($pid == FALSE)
+			{
+				// When adding a new project, there is no icon to start off with. If 
+				// we detect a file for the icon later on, we will replace this.
+				$icon_filename	= '';
+			}
+			else
+			{
+				$icon_filename = $project_model->project_information($pid);
+				$icon_filename = $icon_filename['icon'];
+			}
 
 			$validate = new Validation($this->input->post());
 			$validate->pre_filter('trim');
@@ -201,7 +94,7 @@ class Projects_Controller extends Core_Controller {
 				// First check whether or not we even have an icon to validate.
 				if (!empty($_FILES) && !empty($_FILES['icon']['name']))
 				{
-					// Is there an existing image?
+					// Is there an existing icon?
 					if (!empty($icon_filename))
 					{
 						// Delete the file.
@@ -226,59 +119,101 @@ class Projects_Controller extends Core_Controller {
 					}
 				}
 				
-				// Everything went great! Let's add the project.
-				$project_model->manage_project(array(
-					'uid'			=> $this->uid,
-					'cid'			=> $cid,
-					'name'			=> $name,
-					'website'		=> $website,
-					'contributors'	=> $contributors,
-					'description'	=> $description,
-					'icon'			=> $icon_filename
-					), $pid);
+				if ($pid == FALSE)
+				{
+					// Everything went great! Let's add the project.
+					$project_model->manage_project(array(
+						'uid'			=> $this->uid,
+						'cid'			=> $cid,
+						'name'			=> $name,
+						'website'		=> $website,
+						'contributors'	=> $contributors,
+						'description'	=> $description,
+						'icon'			=> $icon_filename
+						));
+				}
+				else
+				{
+					// Everything went great! Let's edit the project.
+					$project_model->manage_project(array(
+						'uid'			=> $this->uid,
+						'cid'			=> $cid,
+						'name'			=> $name,
+						'website'		=> $website,
+						'contributors'	=> $contributors,
+						'description'	=> $description,
+						'icon'			=> $icon_filename
+						), $pid);
+				}
 
-				// Then load our success view.
-				$project_edit_success_view = new View('project_edit_success');
+				if ($pid == FALSE)
+				{
+					// Then load our success view.
+					$project_success_view = new View('project_success');
+				}
+				else
+				{
+					// Then load our success view.
+					$project_success_view = new View('project_edit_success');
+				}
 
 				// Then generate content.
-				$this->template->content = array($project_edit_success_view);
+				$this->template->content = array($project_success_view);
 			}
 			else
 			{
 				// Errors have occured. Fill in the form and set errors.
-				$project_edit_form_view = new View('project_edit_form');
-				$project_edit_form_view->form = arr::overwrite(array(
+				$project_form_view = new View('project_form');
+				$project_form_view->form = arr::overwrite(array(
 					'name' => '',
 					'website' => '',
 					'contributors' => '',
 					'description' => ''
 					), $validate->as_array());
-				$project_edit_form_view->errors = $validate->errors('project_errors');
-				$project_edit_form_view->pid = $pid;
+				$project_form_view->errors = $validate->errors('project_errors');
+
+				if ($pid != FALSE)
+				{
+					$project_form_view->pid = $pid;
+				}
 
 				// Set project categories.
-				$project_edit_form_view->categories = $project_model->categories();
+				$project_form_view->categories = $project_model->categories();
 
 				// Generate the content.
-				$this->template->content = array($project_edit_form_view);
+				$this->template->content = array($project_form_view);
 			}
 		}
 		else
 		{
 			// Load the neccessary view.
-			$project_edit_form_view = new View('project_edit_form');
+			$project_form_view = new View('project_form');
 
-			// If we didn't press submit, fill up the form with the existing 
-			// info available in the database.
-			$project_edit_form_view->form = $project_model->project_information($pid);
-			$project_edit_form_view->pid = $pid;
+			if ($pid == FALSE)
+			{
+				// If we didn't press submit, we want a blank form.
+				$project_form_view->form = array(
+					'name' => '',
+					'website' => '',
+					'contributors' => '',
+					'description' => ''
+				);
+			}
+			else
+			{
+				// If we didn't press submit, fill up the form with the existing 
+				// info available in the database.
+				$project_form_view->form = $project_model->project_information($pid);
+				$project_form_view->pid = $pid;
+			}
 
 			// Set project categories.
-			$project_edit_form_view->categories = $project_model->categories();
+			$project_form_view->categories = $project_model->categories();
 
 			// Generate the content.
-			$this->template->content = array($project_edit_form_view);
+			$this->template->content = array($project_form_view);
 		}
+
 	}
 
 	/**
