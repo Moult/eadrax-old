10ad90a54465c22b69700637783f7675a9a089a3
Andrew <easylancer@gmail.com>
2009-07-17 16:33:27 +0100

    added in profiles config file to store profile information

---
+++ application/config/profiles.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Eadrax
+ *
+ * Eadrax is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * Eadrax is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *                                                                                
+ * You should have received a copy of the GNU General Public License
+ * along with Eadrax; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @category	Eadrax
+ * @package		Project
+ * @author		Eadrax Team
+ * @copyright	Copyright (C) 2009 Eadrax Team
+ */
+
+// Set the genders
+$config['gender'] = array('Unknown' => 'Please select your gender', 'Female' => 'Female', 'Male' => 'Male');
