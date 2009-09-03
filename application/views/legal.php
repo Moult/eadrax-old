ffd4a1e849c086933d0e6f3c8617b89eac6ac1cb
Dion Moult <dion@thinkmoult.com>
2009-09-03 23:01:45 +0800

    Added legal information view.

---
+++ application/views/legal.php
@@ -0,0 +1,39 @@
+<h2>
+	All that jazz.
+</h2>
+
+<p>
+	LICENSE, CONDITIONS, AND TERMS OF USE FOR THIS WEBSITE AND CONTENT<br />
+
+	You are using this website voluntarily. By using this website, you agree to all of the following terms and conditions. If you do not agree to any of these conditions, you are prohibited from using this website.
+</p>
+
+<p>
+	You will not use this website if any of the following conditions are in violation of any laws in the country, state, territory, or locality in which you legally reside or have accessed this website.
+</p>
+
+<ul>
+	<li> You are at least 16 years of age or older, or the age of legal consent based on the laws in the country, state, territory, or locality in which you legally reside or have accessed this website.</li>
+
+	<li> All website content submitted to this site, is the responsibility of the user who owns the account that submitted it.</li>
+
+	<li>This website and its creators will not claim responsibility over identity theft and misuse of content.</li>
+
+	<li>This website and its creators will comply with requests to take down and put up works which are the subject of controversy.</li>
+
+	<li>All submitted content are automatically for both personal and commercial use, unless stated explicitly by the user submitting the content.</li>
+
+	<li>All website content, including website presentation, design, logos, and source code are copyrighted by the authors. You will not republish, transmit, or distribute the content of this website, in whole or in part, unless specifically allowed by author licensing.</li>
+
+	<li>The materials on this website are provided "as is" and without warranties of any kind, whether expressed or implied, with regard to their correctness, reliability, accuracy, or otherwise.</li>
+
+	<li>This website and its owners, publishers, officers, directors, employees, administrators, editors, and moderators reserve editorial rights (including omission, blocking, and deletion) for all submitted content.</li>
+
+	<li>You grant usage rights, including publication and distribution, to this website and its publishers for any materials you post, upload, submit, or email to this website and its publishers.</li>
+
+	<li>For all services rendered that require payment, payment is non-refundable in neither whole nor part. </li>
+
+	<li>The Eadrax Team reserves the right to vary the terms of conditions without prior notice as they see fit.</li>
+
+	<li>You will not submit any materials which (A) libel, defame, invade privacy, or are obscene or pornographic; (B) infringe on the intellectual property rights, including copyrights, of any third party; (C) violate any law or regulation; (D) advocate illegal activity, including violence and terrorism; (E) advertise or otherwise solicit funds or are a solicitation for goods or services; or (F) contain "malware" (including viruses and worms) or other destructive content.</li>
+</ul>
