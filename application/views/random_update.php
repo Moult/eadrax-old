a30d935d3753aa327f9be621a0e24459747681a9
Dion Moult <dion@thinkmoult.com>
2010-02-12 22:45:55 +0800

    Made the random updates prettier and now shows update summaries.

--- application/views/random_update.php
+++ application/views/random_update.php
@@ -5,14 +5,22 @@
 
 <div style="text-align: center; border: 0px solid #F00; overflow: hidden; width: 820px; margin-left: auto; margin-right: auto;">
 
-<?php foreach ($random_data as $wip) { ?>
-	<div style="float: left; width: 150px; text-align: center; margin-left: 7px; margin-right: 7px;">
+<?php $i = 0; foreach ($random_data as $wip) { $i++; ?>
+<div style="float: left; width: 150px; height: 180px; text-align: center; margin-left: 6px; margin-right: 7px;<?php if ($i != 5) { ?> border-right: 1px solid #EDEDED;<?php } ?>">
+		<p style="font-size: 10px; color: #777;">
+			<?php if ($wip['pid'] != 1) { ?><a href="<?php echo url::base() .'projects/'. $wip['pid'] .'/'; ?>"><?php } ?><strong><?php echo $wip['project_name']; ?></strong><?php if ($wip['pid'] != 1) { ?></a><?php } ?>
+		</p>
+
 		<div style="height: 100px; margin-bottom: 5px; position: relative; text-align: center;">
-			<div style="width: 150px; position: absolute; top: 50px; height: <?php echo $wip['thumb_height']; ?>px; margin-top: -<?php echo $wip['thumb_offset']; ?>px; text-align: center;">
+			<div style="width: 140px; position: absolute; top: 50px; height: <?php echo $wip['thumb_height']; ?>px; margin-top: -<?php echo $wip['thumb_offset']; ?>px; text-align: center;">
 				<a href="<?php echo url::base() .'updates/view/'. $wip['id']; ?>"><img src="<?php echo $wip['filename']; ?>" title="<?php echo $wip['summary']; ?>"></a>
 			</div>
 		</div>
-		<?php if ($wip['pid'] != 1) { ?><a href="<?php echo url::base() .'projects/'. $wip['pid'] .'/'; ?>"><?php } ?><strong><?php echo $wip['project_name']; ?></strong><?php if ($wip['pid'] != 1) { ?></a><?php } ?>
+
+		<p style="word-wrap: break-word; width: 140px;">
+			<a href="<?php echo url::base() .'updates/view/'. $wip['id']; ?>" style="text-decoration: none;"><?php echo $wip['summary']; ?></a>
+		</p>
+
 	</div>
 <?php } ?>
 
