cf2a3a64a61306b5da934071664bea506d5146fd
Dion Moult <dion@thinkmoult.com>
2010-11-13 00:28:16 +0800

    Adding Google Analytics code by default.

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -212,6 +212,20 @@ if (($this->uri->segment(1) == 'updates' && $this->uri->segment(2) == 'add') || 
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="<?php echo url::base(); ?>images/favicon.png" />
         <title>WIPUP</title>
+
+		<!-- Google Analytics -->
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-690493-3']);
+		  _gaq.push(['_setDomainName', '.wipup.org']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
     </head>
 
 	<body>
