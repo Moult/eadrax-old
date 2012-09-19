4b1165ead10a0b10b2d80f5cc04a23987893c0b4
Dion Moult <dion@thinkmoult.com>
2012-09-19 21:07:34 +1000

    Purge everything except for database schema, copyright, and readme.

--- application/views/register_success.php
+++
@@ -1,9 +0,0 @@
-<h2>
-	<img src="<?php echo url::base(); ?>images/icons/thumbs_up_48.png" width="48" height="48" class="icon" alt="" />
-	Welcome to WIPUP!
-</h2>
-
-<p style="text-align: center; font-size: 20px; color: #555; line-height: 50px;">
-	You've just registered an account on WIPUP.<br />
-	Welcome and enjoy :)
-</p>
