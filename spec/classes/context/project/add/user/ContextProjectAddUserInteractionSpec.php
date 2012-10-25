69b47fe3a6805b4a5db5390345010899a30d86a3
Dion Moult <dion@thinkmoult.com>
2012-10-25 22:56:50 +1100

    Before the proposal validates its information, first it should set the author

--- spec/classes/context/project/add/user/ContextProjectAddUserInteractionSpec.php
+++ spec/classes/context/project/add/user/ContextProjectAddUserInteractionSpec.php
@@ -17,17 +17,18 @@ class DescribeContextProjectAddUserInteraction extends \PHPSpec\Context
         })->should->throwException('Exception_Authorisation');
     }
 
-    public function itAsksProposalToValidateInformationIfLoggedIn()
+    public function itAsksProposalToSetItselfAsAuthorIfLoggedIn()
     {
         $module_auth = Mockery::mock('Module_Auth');
         $module_auth->shouldReceive('logged_in')->andReturn('username')->once();
 
         $proposal = Mockery::mock('Proposal');
-        $proposal->shouldReceive('validate_information')->andReturn(TRUE)->once();
+        $proposal->shouldReceive('set_author')->with('Foo')->andReturn(TRUE)->once();
 
         $interaction = Mockery::mock('DescribeContextProjectAddUserInteraction[]');
         $interaction->module_auth = $module_auth;
         $interaction->proposal = $proposal;
+        $interaction->id = 'Foo';
 
         $interaction->authorise_project_add();
     }
