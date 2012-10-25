cf06ad3ef2aa08c9f5379dacd6306403b35e4503
Dion Moult <dion@thinkmoult.com>
2012-10-26 10:16:36 +1100

    Add project model

---
+++ application/classes/model/project.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Eadrax application/classes/model/project.php
+ *
+ * @package   Model
+ * @author    Dion Moult <dion@thinkmoult.com>
+ * @copyright (c) 2012 Dion Moult
+ * @license   ISC http://opensource.org/licenses/isc-license.txt
+ * @link      http://wipup.org/
+ */
+
+defined('SYSPATH') OR die('No direct script access.');
+
+/**
+ * This is a generic WIPUP project.
+ *
+ * @package Model
+ */
+class Model_Project extends Model_Core
+{
+    /** @ignore */
+    public $name;
+    /** @ignore */
+    public $summary;
+    /** @ignore */
+    public $author;
+    /** @ignore */
+    public $description;
+    /** @ignore */
+    public $icon;
+    /** @ignore */
+    public $website;
+    /** @ignore */
+    public $views;
+    /** @ignore */
+    public $last_updated;
+
+    /**
+     * Allows you to set properties whilst instantiating the object.
+     *
+     * $model_project = new Model_Project(array('name' => 'foobar'));
+     *
+     * @param array $properties The list of properties to preset
+     * @return void
+     */
+    public function __construct(Array $properties = array())
+    {
+        foreach ($properties as $property_name => $property_value) {
+            $this->{'set_'.$property_name}($property_value);
+        }
+    }
+
+    /** @ignore */
+    public function get_name()
+    {
+        return $this->name;
+    }
+
+    /** @ignore */
+    public function set_name($name)
+    {
+        $this->name = $name;
+    }
+
+    /** @ignore */
+    public function get_summary()
+    {
+        return $this->summary;
+    }
+
+    /** @ignore */
+    public function set_summary($summary)
+    {
+        $this->summary = $summary;
+    }
+
+    /** @ignore */
+    public function get_author()
+    {
+        return $this->author;
+    }
+
+    /** @ignore */
+    public function set_author(Model_User $author)
+    {
+        $this->author = $author;
+    }
+
+    /** @ignore */
+    public function get_description()
+    {
+        return $this->description;
+    }
+
+    /** @ignore */
+    public function set_description($description)
+    {
+        $this->description = $description;
+    }
+
+    /** @ignore */
+    public function get_icon()
+    {
+        return $this->icon;
+    }
+
+    /** @ignore */
+    public function set_icon($icon)
+    {
+        $this->icon = $icon;
+    }
+
+    /** @ignore */
+    public function get_website()
+    {
+        return $this->website;
+    }
+
+    /** @ignore */
+    public function set_website($website)
+    {
+        $this->website = $website;
+    }
+
+    /** @ignore */
+    public function get_views()
+    {
+        return $this->views;
+    }
+
+    /** @ignore */
+    public function set_views($views)
+    {
+        $this->views = $views;
+    }
+
+    /** @ignore */
+    public function get_last_updated()
+    {
+        return $this->last_updated;
+    }
+
+    /** @ignore */
+    public function set_last_updated($last_updated)
+    {
+        $this->last_updated = $last_updated;
+    }
+}
