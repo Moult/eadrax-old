db4916ed3205831147f24ffdc406f679bd5255c1
Dion Moult <dion@thinkmoult.com>
2012-10-28 00:41:34 +1100

    Delete old spec.

--- spec/classes/exception/ExceptionValidationSpec.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class DescribeExceptionValidation extends \PHPSpec\Context
-{
-    public function itSupportsMultipleExceptions()
-    {
-        $this->spec(new Exception_Validation(array()))->should->beAnInstanceOf('Exception_Multiple');
-    }
-}
