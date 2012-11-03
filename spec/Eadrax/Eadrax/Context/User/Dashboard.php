fcd1b5901f8a54cbc2d8333c4c30349d0a539d15
Dion Moult <dion@thinkmoult.com>
2012-11-03 23:08:52 +1100

    Spec User Dashboard context.
    Warning: this introduces a PHPSpec bug and as a result a lot of specs
    will fail. See TODO comment in code.

---
+++ spec/Eadrax/Eadrax/Context/User/Dashboard.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace spec\Eadrax\Eadrax\Context\User;
+
+require_once 'spec/Eadrax/Eadrax/Context/Core.php';
+
+use PHPSpec2\ObjectBehavior;
+use spec\Eadrax\Eadrax\Context\Core;
+
+class Dashboard extends ObjectBehavior
+{
+    use Core;
+
+    /**
+     * @param Eadrax\Eadrax\Model\User                  $model_user
+     * @param Eadrax\Eadrax\Context\User\Dashboard\User $role_user
+     * @param Eadrax\Eadrax\Entity\Auth                 $entity_auth
+     */
+    function let($model_user, $role_user, $entity_auth)
+    {
+        $role_user->assign_data($model_user)->shouldBeCalled();
+        // TODO: This is throwing errors due to a PHPSpec2 bug.
+        // @link https://github.com/phpspec/phpspec2/issues/62
+        $role_user->link(array(
+            'entity_auth' => $entity_auth
+        ))->shouldBeCalled();
+        $this->beConstructedWith($model_user, $role_user, $entity_auth);
+    }
+
+    function it_should_be_initializable()
+    {
+        $this->shouldHaveType('Eadrax\Eadrax\Context\User\Dashboard');
+    }
+
+    function it_should_be_a_context()
+    {
+        $this->shouldHaveType('Eadrax\Eadrax\Context\Core');
+    }
+
+    function it_catches_authorisation_exceptions_during_usecase($role_user)
+    {
+        $role_user->authorise_dashboard()->willThrow('Eadrax\Eadrax\Exception\Authorisation', 'foo');
+        $this->execute()->shouldBe(array(
+            'status' => 'failure',
+            'type' => 'authorisation',
+            'data' => array(
+                'errors' => array('foo')
+            )
+        ));
+    }
+
+    function it_executes_the_usecase_successfully($role_user)
+    {
+        $role_user->authorise_dashboard()->willReturn('foo');
+        $this->execute()->shouldBe(array(
+            'status' => 'success',
+            'data' => 'foo'
+        ));
+    }
+}
--- src/Eadrax/Eadrax/Context/User/Dashboard.php
+++ src/Eadrax/Eadrax/Context/User/Dashboard.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Eadrax application/classes/Context/User/Dashboard.php
+ * Eadrax Context/User/Dashboard.php
  *
  * @package   Context
  * @author    Dion Moult <dion@thinkmoult.com>
@@ -9,14 +9,17 @@
  * @link      http://wipup.org/
  */
 
-defined('SYSPATH') OR die('No direct script access.');
+namespace Eadrax\Eadrax\Context\User;
+use Eadrax\Eadrax\Context\Core;
+use Eadrax\Eadrax\Context\User\Dashboard\User;
+use Eadrax\Eadrax\Exception;
 
 /**
  * Enacts the usecase for user dashboard.
  *
  * @package Context
  */
-class Context_User_Dashboard extends Context_Core
+class Dashboard extends Core
 {
     /**
      * User role
@@ -29,15 +32,17 @@ class Context_User_Dashboard extends Context_Core
      * dependencies.
      *
      * @param Model_User  $model_user  User data object
-     * @param Module_Auth $module_auth Authentication system
+     * @param Role_User   $role_user   The user role
+     * @param Entity_Auth $entity_auth Authentication system
      * @return void
      */
-    public function __construct($model_user, $module_auth)
+    public function __construct($model_user, $role_user, $entity_auth)
     {
-        $this->user = new Context_User_Dashboard_User($model_user);
-        $this->user->link(array(
-            'module_auth' => $module_auth
+        $role_user->assign_data($model_user);
+        $role_user->link(array(
+            'entity_auth' => $entity_auth
         ));
+        $this->user = $role_user;
     }
 
     /**
@@ -51,7 +56,7 @@ class Context_User_Dashboard extends Context_Core
         {
             $data = $this->user->authorise_dashboard();
         }
-        catch (Exception_Authorisation $e)
+        catch (Exception\Authorisation $e)
         {
             return array(
                 'status' => 'failure',
