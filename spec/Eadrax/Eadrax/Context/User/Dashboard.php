8296b8fc217f8a07e37912eabbc3660a6d844d7c
Dion Moult <dion@thinkmoult.com>
2012-11-04 10:36:08 +1100

    Remove pending exceptions
    Errors fixed in PHPSpec2 develop branch.

--- spec/Eadrax/Eadrax/Context/User/Dashboard.php
+++ spec/Eadrax/Eadrax/Context/User/Dashboard.php
@@ -18,10 +18,7 @@ class Dashboard extends ObjectBehavior
      */
     function let($model_user, $role_user, $entity_auth)
     {
-        throw new \PHPSpec2\Exception\Example\PendingException('Waiting for bug #62');
         $role_user->assign_data($model_user)->shouldBeCalled();
-        // TODO: This is throwing errors due to a PHPSpec2 bug.
-        // @link https://github.com/phpspec/phpspec2/issues/62
         $role_user->link(array(
             'entity_auth' => $entity_auth
         ))->shouldBeCalled();
@@ -30,19 +27,16 @@ class Dashboard extends ObjectBehavior
 
     function it_should_be_initializable()
     {
-        throw new \PHPSpec2\Exception\Example\PendingException('Waiting for bug #62');
         $this->shouldHaveType('Eadrax\Eadrax\Context\User\Dashboard');
     }
 
     function it_should_be_a_context()
     {
-        throw new \PHPSpec2\Exception\Example\PendingException('Waiting for bug #62');
         $this->shouldHaveType('Eadrax\Eadrax\Context\Core');
     }
 
     function it_catches_authorisation_exceptions_during_usecase($role_user)
     {
-        throw new \PHPSpec2\Exception\Example\PendingException('Waiting for bug #62');
         $role_user->authorise_dashboard()->willThrow('Eadrax\Eadrax\Exception\Authorisation', 'foo');
         $this->execute()->shouldBe(array(
             'status' => 'failure',
@@ -55,7 +49,6 @@ class Dashboard extends ObjectBehavior
 
     function it_executes_the_usecase_successfully($role_user)
     {
-        throw new \PHPSpec2\Exception\Example\PendingException('Waiting for bug #62');
         $role_user->authorise_dashboard()->willReturn('foo');
         $this->execute()->shouldBe(array(
             'status' => 'success',
