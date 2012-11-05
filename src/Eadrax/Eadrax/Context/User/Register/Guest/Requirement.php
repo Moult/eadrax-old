f26e7bc151eabb1c9cb640e155b622ca8085b0a4
Dion Moult <dion@thinkmoult.com>
2012-11-05 19:21:30 +1100

    Port guest role requirement for user register context.

--- src/Eadrax/Eadrax/Context/User/Register/Guest/Requirement.php
+++ src/Eadrax/Eadrax/Context/User/Register/Guest/Requirement.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Eadrax application/classes/Context/User/Register/Guest/Requirement.php
+ * Eadrax Context/User/Register/Guest/Requirement.php
  *
  * @package   Context
  * @author    Dion Moult <dion@thinkmoult.com>
@@ -9,7 +9,7 @@
  * @link      http://wipup.org/
  */
 
-defined('SYSPATH') OR die('No direct script access.');
+namespace Eadrax\Eadrax\Context\User\Register\Guest;
 
 /**
  * Define data model prerequisites to play the Guest role
@@ -17,7 +17,7 @@ defined('SYSPATH') OR die('No direct script access.');
  * @package    Context
  * @subpackage Role
  */
-interface Context_User_Register_Guest_Requirement
+interface Requirement
 {
     /** @ignore */
     public function get_username();
