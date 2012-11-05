2ec604f056c840762dab14c6e2bd8bb28783aa71
Dion Moult <dion@thinkmoult.com>
2012-11-05 19:23:55 +1100

    Add repository for user register context.

---
+++ src/Eadrax/Eadrax/Context/User/Register/Repository.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Eadrax Context/User/Register/Repository.php
+ *
+ * @package   Context
+ * @author    Dion Moult <dion@thinkmoult.com>
+ * @copyright (c) 2012 Dion Moult
+ * @license   ISC http://opensource.org/licenses/isc-license.txt
+ * @link      http://wipup.org/
+ */
+
+namespace Eadrax\Eadrax\Context\User\Register;
+
+/**
+ * Handles persistance during user registration.
+ *
+ * @package    Context
+ * @subpackage Repository
+ */
+interface Repository
+{
+    /**
+     * Saves a user
+     *
+     * @param Model_User $model_user The user to save
+     * @return void
+     */
+    public function register($model_user);
+
+    /**
+     * Checks whether or not a username is unique
+     *
+     * @param string $username The username to check
+     * @return bool
+     */
+    public function is_unique_username($username);
+}
