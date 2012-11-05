67b1244bb8ee76d8311bced2e594784b93ba06ba
Dion Moult <dion@thinkmoult.com>
2012-11-05 22:13:28 +1100

    Spec user role for project add context

---
+++ spec/Eadrax/Eadrax/Context/Project/Add/User.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace spec\Eadrax\Eadrax\Context\Project\Add;
+
+use PHPSpec2\ObjectBehavior;
+
+class User extends ObjectBehavior
+{
+    /**
+     * @param Eadrax\Eadrax\Entity\Auth $entity_auth
+     */
+    function let($entity_auth)
+    {
+        $model_user = new \Eadrax\Eadrax\Model\User;
+        $model_user->id = 'foo';
+        $this->beConstructedWith($model_user, $entity_auth);
+    }
+
+    function it_should_be_initializable()
+    {
+        $this->shouldHaveType('Eadrax\Eadrax\Context\Project\Add\User');
+    }
+
+    function it_is_a_user_role()
+    {
+        $this->shouldHaveType('Eadrax\Eadrax\Model\User');
+        $this->shouldHaveType('Eadrax\Eadrax\Context\Project\Add\User\Requirement');
+    }
+
+    function it_should_be_able_to_construct_data()
+    {
+        $this->get_username()->shouldBe(NULL);
+        $this->get_password()->shouldBe(NULL);
+        $this->get_email()->shouldBe(NULL);
+        $this->get_id()->shouldBe('foo');
+    }
+
+    function it_should_construct_links()
+    {
+        $this->entity_auth->shouldHaveType('Eadrax\Eadrax\Entity\Auth');
+    }
+
+    function it_should_be_able_to_import_data_from_a_user_model()
+    {
+        $model_user = new \Eadrax\Eadrax\Model\User;
+        $model_user->id = 'bar';
+        $this->assign_data($model_user);
+        $this->get_id()->shouldBe('bar');
+    }
+
+}
---
+++ src/Eadrax/Eadrax/Context/Project/Add/User.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Eadrax Context/Project/Add/User.php
+ *
+ * @package   Context
+ * @author    Dion Moult <dion@thinkmoult.com>
+ * @copyright (c) 2012 Dion Moult
+ * @license   ISC http://opensource.org/licenses/isc-license.txt
+ * @link      http://wipup.org/
+ */
+
+namespace Eadrax\Eadrax\Context\Project\Add;
+use Eadrax\Eadrax\Context;
+use Eadrax\Eadrax\Model;
+use Eadrax\Eadrax\Entity;
+
+/**
+ * Allows model_user to be cast as a user role
+ *
+ * @package    Context
+ * @subpackage Role
+ */ 
+class User extends Model\User implements User\Requirement
+{
+    use Context\Interaction, User\Interaction;
+
+    /**
+     * Takes a data object and copies all of its properties
+     *
+     * @param Model\User  $model_user  Data object to copy
+     * @param Entity\Auth $entity_auth The authentication entity
+     * @return void
+     */
+    public function __construct(Model\User $model_user = NULL, Entity\Auth $entity_auth = NULL)
+    {
+        if ($model_user !== NULL)
+        {
+            $this->assign_data($model_user);
+        }
+
+        if ($entity_auth !== NULL)
+        {
+            $this->link(array(
+                'entity_auth' => $entity_auth
+            ));
+        }
+    }
+
+    /**
+     * Loads in data from a model
+     *
+     * @param Model\User $model_user The user model to load.
+     * @return void
+     */
+    public function assign_data(Model\User $model_user)
+    {
+        parent::__construct(get_object_vars($model_user));
+    }
+}
