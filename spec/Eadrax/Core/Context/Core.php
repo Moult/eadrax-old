6a0ced616283ef58764d4cc40f6326784e24e422
Dion Moult <dion@thinkmoult.com>
2012-12-08 14:50:02 +1100

    Add tests for whether or not it is a context.

--- spec/Eadrax/Core/Context/Core.php
+++ spec/Eadrax/Core/Context/Core.php
@@ -2,4 +2,9 @@
 
 namespace spec\Eadrax\Core\Context;
 
-trait Core {}
+trait Core {
+    function it_is_a_core_context()
+    {
+        $this->shouldHaveType('\Eadrax\Core\Context\Core');
+    }
+}
