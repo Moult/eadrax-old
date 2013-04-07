c431f117e9a66dc9563edd353429babbffb157aa
Dion Moult <dion@thinkmoult.com>
2013-04-08 08:59:39 +1000

    Add get_username_and_email to repository

--- src/Eadrax/Core/Usecase/User/Track/Repository.php
+++ src/Eadrax/Core/Usecase/User/Track/Repository.php
@@ -11,4 +11,8 @@ interface Repository
     public function is_fan_of($fan, $idol);
     public function add_idol($fan, $idol);
     public function remove_idol($fan, $idol);
+    /**
+     * @return Eadrax\Core\Data\User
+     */
+    public function get_username_and_email($user_id);
 }
