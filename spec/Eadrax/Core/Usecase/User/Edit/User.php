7cf1ab0a45747dbb516e9bda85acda4a7a9bfe1b
Dion Moult <dion@thinkmoult.com>
2013-04-07 19:50:39 +1000

    Add user role in edit user usecase

--- spec/Eadrax/Core/Usecase/User/Edit/User.php
+++ spec/Eadrax/Core/Usecase/User/Edit/User.php
@@ -6,8 +6,103 @@ use PHPSpec2\ObjectBehavior;
 
 class User extends ObjectBehavior
 {
+    /**
+     * @param Eadrax\Core\Data\File $avatar
+     * @param Eadrax\Core\Data\User $user
+     * @param Eadrax\Core\Data\User $auth_user
+     * @param Eadrax\Core\Usecase\User\Edit\Repository $repository
+     * @param Eadrax\Core\Tool\Auth $auth
+     * @param Eadrax\Core\Tool\Validation $validation
+     */
+    function let($avatar, $user, $auth_user, $repository, $auth, $validation)
+    {
+        $auth->get_user()->shouldBeCalled()->willReturn($auth_user);
+        $auth_user->id = 'id';
+        $user->password = 'password';
+        $user->password_verify = 'password_verify';
+        $user->email = 'email@address.com';
+        $user->bio = 'bio';
+        $user->website = 'http://website.com';
+        $user->location = 'location';
+        $user->avatar = $avatar;
+        $user->dob = 'dob';
+        $user->gender = 'gender';
+        $user->show_email = TRUE;
+        $user->receive_notifications = TRUE;
+
+        $this->beConstructedWith($user, $repository, $auth, $validation);
+
+        $this->id->shouldBe('id');
+        $this->password->shouldBe('password');
+        $this->password_verify->shouldBe('password_verify');
+        $this->email->shouldBe('email@address.com');
+        $this->bio->shouldBe('bio');
+        $this->website->shouldBe('http://website.com');
+        $this->location->shouldBe('location');
+        $this->avatar->shouldBe($avatar);
+        $this->dob->shouldBe('dob');
+        $this->gender->shouldBe('gender');
+        $this->show_email->shouldBe(TRUE);
+        $this->receive_notifications->shouldBe(TRUE);
+    }
+
     function it_should_be_initializable()
     {
         $this->shouldHaveType('Eadrax\Core\Usecase\User\Edit\User');
+    }
+
+    function it_should_be_a_user_data()
+    {
+        $this->shouldHaveType('Eadrax\Core\Data\User');
+    }
+
+    function it_authorises_logged_in_users($auth)
+    {
+        $auth->logged_in()->shouldBeCalled()->willReturn(TRUE);
+        $this->authorise();
+    }
+
+    function it_does_not_authorise_guests($auth)
+    {
+        $auth->logged_in()->shouldBeCalled()->willReturn(FALSE);
+        $this->shouldThrow('Eadrax\Core\Exception\Authorisation')
+            ->duringAuthorise();
+    }
+
+    function it_checks_for_valid_user_information($validation)
+    {
+        $validation->check()->shouldBeCalled()->willReturn(TRUE);
+        $this->validate();
+    }
+
+    function it_checks_for_invalid_user_information($validation, $avatar)
+    {
+        $validation->setup(array(
+            'password' => 'password',
+            'email' => 'email@address.com',
+            'website' => 'http://website.com',
+            'avatar' => $avatar,
+            'dob' => 'dob'
+        ))->shouldBeCalled();
+        $validation->rule('password', 'not_empty')->shouldBeCalled();
+        $validation->rule('password', 'min_length', '6')->shouldBeCalled();
+        $validation->rule('password', 'matches', 'password_verify')->shouldBeCalled();
+        $validation->rule('email', 'not_empty')->shouldBeCalled();
+        $validation->rule('email', 'email')->shouldBeCalled();
+        $validation->rule('website', 'url')->shouldBeCalled();
+        $validation->rule('avatar', 'upload_valid')->shouldBeCalled();
+        $validation->rule('avatar', 'upload_type', array('jpg', 'png'))->shouldBeCalled();
+        $validation->rule('avatar', 'upload_size', '1M')->shouldBeCalled();
+        $validation->rule('dob', 'date')->shouldBeCalled();
+        $validation->check()->shouldBeCalled()->willReturn(FALSE);
+        $validation->errors()->willReturn(array('foo'));
+        $this->shouldThrow('Eadrax\Core\Exception\Validation')
+            ->duringValidate();
+    }
+
+    function it_updates_the_users_data($repository)
+    {
+        $repository->edit_user($this)->shouldBeCalled();
+        $this->update();
     }
 }
--- src/Eadrax/Core/Usecase/User/Edit/User.php
+++ src/Eadrax/Core/Usecase/User/Edit/User.php
@@ -1,7 +1,75 @@
 <?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
 
 namespace Eadrax\Core\Usecase\User\Edit;
+use Eadrax\Core\Data;
+use Eadrax\Core\Tool;
+use Eadrax\Core\Exception;
 
-class User
+class User extends Data\User
 {
+    public function __construct(Data\User $user, Repository $repository, Tool\Auth $auth, Tool\Validation $validation)
+    {
+        $auth_user = $auth->get_user();
+        $this->id = $auth_user->id;
+
+        $this->password = $user->password;
+        $this->password_verify = $user->password_verify;
+        $this->email = $user->email;
+        $this->bio = $user->bio;
+        $this->website = $user->website;
+        $this->location = $user->location;
+        $this->avatar = $user->avatar;
+        $this->dob = $user->dob;
+        $this->gender = $user->gender;
+        $this->show_email = $user->show_email;
+        $this->receive_notifications = $user->receive_notifications;
+
+        $this->repository = $repository;
+        $this->auth = $auth;
+        $this->validation = $validation;
+    }
+
+    public function authorise()
+    {
+        if ( ! $this->auth->logged_in())
+            throw new Exception\Authorisation('You need to be logged in.');
+    }
+
+    public function validate()
+    {
+        $this->setup_validation();
+
+        if ( ! $this->validation->check())
+            throw new Exception\Validation($this->validation->errors());
+    }
+
+    private function setup_validation()
+    {
+        $this->validation->setup(array(
+            'password' => $this->password,
+            'email' => $this->email,
+            'website' => $this->website,
+            'avatar' => $this->avatar,
+            'dob' => $this->dob
+        ));
+        $this->validation->rule('password', 'not_empty');
+        $this->validation->rule('password', 'min_length', '6');
+        $this->validation->rule('password', 'matches', 'password_verify');
+        $this->validation->rule('email', 'not_empty');
+        $this->validation->rule('email', 'email');
+        $this->validation->rule('website', 'url');
+        $this->validation->rule('avatar', 'upload_valid');
+        $this->validation->rule('avatar', 'upload_type', array('jpg', 'png'));
+        $this->validation->rule('avatar', 'upload_size', '1M');
+        $this->validation->rule('dob', 'date');
+    }
+
+    public function update()
+    {
+        $this->repository->edit_user($this);
+    }
 }
