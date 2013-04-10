931a7a272842236cbd5d1e7e0de2a0e2b56ae561
Dion Moult <dion@thinkmoult.com>
2013-04-10 11:01:04 +1000

    Add service role in hook add usecase

--- spec/Eadrax/Core/Usecase/Hook/Add/Service.php
+++ spec/Eadrax/Core/Usecase/Hook/Add/Service.php
@@ -6,8 +6,40 @@ use PHPSpec2\ObjectBehavior;
 
 class Service extends ObjectBehavior
 {
+    /**
+     * @param Eadrax\Core\Data\Hook $hook
+     * @param Eadrax\Core\Tool\Validation $validation
+     */
+    function let($hook, $validation)
+    {
+        $hook->url = 'hook_url';
+        $this->beConstructedWith($hook, $validation);
+        $this->url->shouldBe('hook_url');
+    }
+
     function it_should_be_initializable()
     {
         $this->shouldHaveType('Eadrax\Core\Usecase\Hook\Add\Service');
+    }
+
+    function it_should_be_a_hook()
+    {
+        $this->shouldHaveType('Eadrax\Core\Data\Hook');
+    }
+
+    function it_should_validate_whether_it_is_valid($validation)
+    {
+        $validation->setup(array('url' => 'hook_url'))->shouldBeCalled();
+        $validation->rule('url', 'rss2')->shouldBeCalled();
+        $validation->check()->shouldBeCalled()->willReturn(TRUE);
+        $this->is_valid();
+    }
+
+    function it_invalidates_fake_hooks($validation)
+    {
+        $validation->check()->shouldBeCalled()->willReturn(FALSE);
+        $validation->errors()->shouldBeCalled()->willReturn(array('url'));
+        $this->shouldThrow('Eadrax\Core\Exception\Validation')
+            ->duringIs_valid();
     }
 }
--- src/Eadrax/Core/Usecase/Hook/Add/Service.php
+++ src/Eadrax/Core/Usecase/Hook/Add/Service.php
@@ -1,7 +1,27 @@
 <?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
 
 namespace Eadrax\Core\Usecase\Hook\Add;
+use Eadrax\Core\Data;
+use Eadrax\Core\Tool;
+use Eadrax\Core\Exception;
 
-class Service
+class Service extends Data\Hook
 {
+    public function __construct(Data\Hook $hook, Tool\Validation $validation)
+    {
+        $this->url = $hook->url;
+        $this->validation = $validation;
+    }
+
+    public function is_valid()
+    {
+        $this->validation->setup(array('url' => $this->url));
+        $this->validation->rule('url', 'rss2');
+        if ( ! $this->validation->check())
+            throw new Exception\Validation($this->validation->errors());
+    }
 }
