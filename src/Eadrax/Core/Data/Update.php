83935229c4b3bca9ba95ffe32b1316e44b937867
Dion Moult <dion@thinkmoult.com>
2013-04-13 11:10:22 +1000

    Add update data type

---
+++ src/Eadrax/Core/Data/Update.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+namespace Eadrax\Core\Data;
+
+class Update
+{
+    public $id;
+    public $type;
+    public $content;
+    public $extra;
+    public $private;
+    public $project;
+}
