a043f28be4e2523fed1a4cd20f38a628e69f1c57
Dion Moult <dion@thinkmoult.com>
2013-04-13 12:01:52 +1000

    Begin coding proposal role in update add usecase
    Can validate normal text and pastes so far

--- spec/Eadrax/Core/Usecase/Update/Add/Proposal.php
+++ spec/Eadrax/Core/Usecase/Update/Add/Proposal.php
@@ -6,8 +6,70 @@ use PHPSpec2\ObjectBehavior;
 
 class Proposal extends ObjectBehavior
 {
-    function it_should_be_initializable()
+    /**
+     * @param Eadrax\Core\Data\Update $update
+     * @param Eadrax\Core\Data\Project $project
+     * @param Eadrax\Core\Tool\Validation $validation
+     */
+    public function let($update, $project, $validation)
+    {
+        $update->type = 'type';
+        $update->content = 'content';
+        $update->extra = 'extra';
+        $update->private = 'private';
+        $update->project = $project;
+        $this->beConstructedWith($update, $validation);
+    }
+
+    function it_should_be_initializable($project)
     {
         $this->shouldHaveType('Eadrax\Core\Usecase\Update\Add\Proposal');
+        $this->type->shouldBe('type');
+        $this->content->shouldBe('content');
+        $this->extra->shouldBe('extra');
+        $this->private->shouldBe('private');
+        $this->project->shouldBe($project);
+    }
+
+    function it_should_be_an_update()
+    {
+        $this->shouldHaveType('Eadrax\Core\Data\Update');
+    }
+
+    function it_validates_text_updates($update, $validation)
+    {
+        $update->type = 'text';
+        $update->content = 'Foobar';
+        $validation->setup(array('content' => 'Foobar'))->shouldBeCalled();
+        $validation->rule('content', 'not_empty')->shouldBeCalled();
+        $validation->check()->shouldBeCalled()->willReturn(FALSE);
+        $validation->errors()->shouldBeCalled()->willReturn(array('content'));
+        $this->shouldThrow('Eadrax\Core\Exception\Validation')
+            ->duringValidate();
+        $this->extra->shouldBe(NULL);
+    }
+
+    function it_validates_paste_updates($update, $validation)
+    {
+        $update->type = 'paste';
+        $update->extra = 'bash';
+        $update->content = '#! /bin/bash';
+        $validation->setup(array(
+            'content' => '#! /bin/bash',
+            'syntax' => 'bash'
+        ))->shouldBeCalled();
+        $validation->rule('content', 'not_empty')->shouldBeCalled();
+        $validation->rule('syntax', 'not_empty')->shouldBeCalled();
+        $validation->callback('syntax', array($this, 'validate_paste_syntax'), array('syntax'))->shouldBeCalled();
+        $validation->check()->shouldBeCalled()->willReturn(FALSE);
+        $validation->errors()->shouldBeCalled()->willReturn(array('content'));
+        $this->shouldThrow('Eadrax\Core\Exception\Validation')
+            ->duringValidate();
+    }
+
+    function it_validates_paste_syntax()
+    {
+        $this->validate_paste_syntax('bash')->shouldReturn(TRUE);
+        $this->validate_paste_syntax('english')->shouldReturn(FALSE);
     }
 }
--- src/Eadrax/Core/Usecase/Update/Add/Proposal.php
+++ src/Eadrax/Core/Usecase/Update/Add/Proposal.php
@@ -1,7 +1,69 @@
 <?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
 
 namespace Eadrax\Core\Usecase\Update\Add;
+use Eadrax\Core\Data;
+use Eadrax\Core\Tool;
+use Eadrax\Core\Exception;
 
-class Proposal
+class Proposal extends Data\Update
 {
+    private $validation;
+
+    public function __construct(Data\Update $update, Tool\Validation $validation)
+    {
+        $this->type = $update->type;
+        $this->content = $update->content;
+        $this->extra = $update->extra;
+        $this->private = $update->private;
+        $this->project = $update->project;
+
+        $this->validation = $validation;
+    }
+
+    public function validate()
+    {
+        if ($this->type === 'text')
+        {
+            $this->validate_text();
+        }
+        elseif ($this->type === 'paste')
+        {
+            $this->validate_paste();
+        }
+
+        if ( ! $this->validation->check())
+            throw new Exception\Validation($this->validation->errors());
+    }
+
+    private function validate_text()
+    {
+        $this->validation->setup(array('content' => $this->content));
+        $this->validation->rule('content', 'not_empty');
+        $this->extra = NULL;
+    }
+
+    private function validate_paste()
+    {
+        $this->validation->setup(array(
+            'content' => $this->content,
+            'syntax' => $this->extra
+        ));
+        $this->validation->rule('content', 'not_empty');
+        $this->validation->rule('syntax', 'not_empty');
+        $this->validation->callback('syntax', array($this, 'validate_paste_syntax'), array('syntax'));
+    }
+
+    public function validate_paste_syntax($syntax)
+    {
+        $supported_syntaxes = array('none', 'abap', 'actionscript', 'actionscript3', 'ada', 'apache', 'applescript', 'apt_sources', 'asm', 'asp', 'autoit', 'avisynth', 'bash', 'basic4gl', 'bf', 'blitzbasic', 'bnf', 'boo', 'c', 'c_mac', 'caddcl', 'cadlisp', 'cfdg', 'cfm', 'cil', 'cobol', 'cpp', 'cpp-qt', 'csharp', 'css', 'd', 'dcs', 'delphi', 'diff', 'div', 'dos', 'dot', 'eiffel', 'email', 'fortran', 'freebasic', 'genero', 'gettext', 'glsl', 'gml', 'gnuplot', 'groovy', 'haskell', 'hq9plus', 'html4strict', 'idl', 'ini', 'inno', 'intercal', 'io', 'java', 'java5', 'javascript', 'kixtart', 'klonec', 'klonecpp', 'latex', 'lisp', 'lolcode', 'lotusformulas', 'lotusscript', 'lscript', 'lua', 'm68k', 'make', 'matlab', 'mirc', 'modula3', 'mpasm', 'mxml', 'mysql', 'nsis', 'objc', 'ocaml', 'ocaml-brief', 'oobas', 'oracle11', 'oracle8', 'pascal', 'per', 'perl', 'php', 'php-brief', 'pic16', 'pixelbender', 'plsql', 'povray', 'powershell', 'progress', 'prolog', 'providex', 'python', 'qbasic', 'rails', 'rebol', 'reg', 'robots', 'ruby', 'sas', 'scala', 'scheme', 'scilab', 'sdlbasic', 'smalltalk', 'smarty', 'sql', 'tcl', 'teraterm', 'text', 'thinbasic', 'tsql', 'typoscript', 'vb', 'vbnet', 'verilog', 'vhdl', 'vim', 'visualfoxpro', 'visualprolog', 'whitespace', 'whois', 'winbatch', 'xml', 'xorg_conf', 'xpp', 'z80');
+
+        if (in_array($syntax, $supported_syntaxes))
+            return TRUE;
+        else
+            return FALSE;
+    }
 }
