7e50623611ea4f867ae8786c93973ae3c5a9cfb1
Dion Moult <dion@thinkmoult.com>
2013-04-14 12:36:13 +1000

    Add upload tool

---
+++ src/Eadrax/Core/Tool/Upload.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+namespace Eadrax\Core\Tool;
+use Eadrax\Core\Data;
+
+interface Upload
+{
+    /**
+     * Saves an uploaded file to a specific location.
+     *
+     * Automatically prefixes filename with unique identifier.
+     *
+     * Example:
+     * $upload->save($file, '/path/to/upload');
+     *
+     * @return mixed string bool full path to uploaded file or FALSE if failed
+     */
+    public function save(Data\File $file, $directory);
+}
