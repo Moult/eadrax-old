80a1a54a8d65132f2a7296e56f0681bb99ff9aa8
Dion Moult <dion@thinkmoult.com>
2013-07-14 02:06:08 +1000

    Forgot to fully spec new repository requirements

--- src/Eadrax/Core/Usecase/Update/Add/Repository.php
+++ src/Eadrax/Core/Usecase/Update/Add/Repository.php
@@ -17,4 +17,24 @@ interface Repository
      * @return int Unique ID of the saved update
      */
     public function save_text($project_id, $update_private, $text_message);
+
+    /**
+     * @return int Unique ID of the saved update
+     */
+    public function save_paste($project_id, $update_private, $paste_text, $paste_syntax);
+
+    /**
+     * @return string Path to saved file
+     */
+    public function save_file($name, $tmp_name, $type, $size, $error);
+
+    /**
+     * @return string Path to saved file
+     */
+    public function save_generated_file($tmp_name);
+
+    /**
+     * @return int Unique ID of the saved update
+     */
+    public function save_image($project_id, $update_private, $image_file_path, $image_thumbnail_path, $image_width, $image_height);
 }
